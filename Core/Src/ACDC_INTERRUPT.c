4d3fe8abd0b74d055e362710be1c06c95ec43bde
Devin Marx <106412270+DevinJM3@users.noreply.github.com>
2024-01-14 11:31:11 -0600

    Created STM32_CI.yml and modified accompanying files
    ### Create STM32_CI.yml, Updated Makefile, and Updated stm32f1xx.h
    * Created the STM32_CI.yml file which installs dependencies (ARM GCC, Make, and CppCheck) and then compiles and runs static analysis on the project.
    * Updated Makefile so now there is a command to run CppCheck
    * Updated stm32f1xx.h so that the STM32F103xB define is used
    ### Fixed static analysis errors and changed  GPIO_TriggerType enum names
    * Fixed static analysis errors by setting a few more function parameters to const
    * Changed GPIO_TriggerType enum names so that they will be easier when programming, now they are all prefixed with TT_

--- Core/Src/ACDC_INTERRUPT.c
+++ Core/Src/ACDC_INTERRUPT.c
@@ -1,20 +1,20 @@
 #include "ACDC_INTERRUPT.h"
 #include "ACDC_GPIO.h"
 
-void GPIO_INT_SetToInterrupt(GPIO_TypeDef *GPIOx, uint16_t GPIO_PIN, GPIO_TriggerType triggerType){
+void GPIO_INT_SetToInterrupt(const GPIO_TypeDef *GPIOx, uint16_t GPIO_PIN, GPIO_TriggerType triggerType){
     //External Interrupts are Muxed so you can only use 16 concurrently {See Page 210}
 
     RCC->APB2ENR |= RCC_APB2ENR_AFIOEN; //Enable Alternate Functions (Needed for Interrupts)
 
     uint16_t pin = GPIO_GetPinNumber(GPIO_PIN);
     EXTI->IMR |= 1 << pin;        //Enable Interrupts on pin
-    if(triggerType & RISING_EDGE)
+    if(triggerType & TT_RISING_EDGE)
         EXTI->RTSR |= 1 << pin;   //Enable RisingEdge Interrupts on pin
-    if(triggerType & FALLING_EDGE)
+    if(triggerType & TT_FALLING_EDGE)
         EXTI->FTSR |= 1 << pin;   //Enable FallingEdge Interrupts on pin
 
     uint16_t CRNumber = pin >> 2; //Gets the EXTICRx number
-    uint16_t GpioMask;            //Gets the GPIO mask {See Page 191}
+    uint16_t GpioMask = 0;        //Gets the GPIO mask {See Page 191}
     if(GPIOx == GPIOA)
         GpioMask = 0b0000;
     else if(GPIOx == GPIOB)
