8fe7a7810bbac7ba840d5e960533e2c4ca10a76f
DevinJM3 <106412270+DevinJM3@users.noreply.github.com>
2024-01-19 16:53:12 -0600

    Fixed #7. Fixed issue that prevented higher clock speeds from working.

--- Core/Src/ACDC_CLOCK.c
+++ Core/Src/ACDC_CLOCK.c
@@ -12,6 +12,11 @@ static void DisableHSI_EnablePLL(void);
 /// @brief Enables the HSI and disables the PLL from the SysClock Mux
 static void EnableHSI_DisablePLL(void);
 
+/// @brief Sets the Flash memory speed on the CPU {See RM-58, RM-59, RM-60}
+/// @param SCS_x Desired Clock Speed of the System
+/// @param RCC_CFGR_HPRE_x AHB Prescaler Value
+static void SetFlashMemorySpeed(SystemClockSpeed SCS_x, uint32_t RCC_CFGR_HPRE_x);
+
 void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
 
     RCC->CFGR |= RCC_CFGR_PLLSRC;   // Use HSE as PLL Source,
@@ -21,12 +26,6 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
     RCC->CFGR &= ~(RCC_CFGR_PLLXTPRE_Msk | RCC_CFGR_PLLMULL_Msk | RCC_CFGR_HPRE_Msk | RCC_CFGR_PPRE1_Msk);
 
     switch (SCS_x){
-        case SCS_72MHz: //PLLMULL9
-            RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-            RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE;
-            RCC->CFGR |= RCC_CFGR_PLLMULL9;    //Should be 9 but still doesnt work still too fast
-        break;
-
         case SCS_16MHz: //PLLMULL2
         case SCS_24MHz: //PLLMULL3
         case SCS_32MHz: //PLLMULL4
@@ -34,10 +33,11 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
         case SCS_48MHz: //PLLMULL6
         case SCS_56MHz: //PLLMULL7
         case SCS_64MHz: //PLLMULL8
-        //case SCS_72MHz: //PLLMULL9  //Broken
+        case SCS_72MHz: //PLLMULL9
+            SetFlashMemorySpeed(SCS_x, RCC_CFGR_HPRE_DIV1);
             RCC->CFGR |= RCC_CFGR_HPRE_DIV1;                            // AHB Prescaler DIV1
             RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE;                         // HSE No DIV into PLL Src Mux
-            RCC->CFGR |= (SCS_x / 4000000 - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier
+            RCC->CFGR |= (SCS_x / 8000000 - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier
         break;
 
         case SCS_8MHz:  //PLLMULL2
@@ -48,6 +48,7 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
         case SCS_44MHz: //PLLMULL11
         case SCS_52MHz: //PLLMULL13
         case SCS_60MHz: //PLLMULL15
+            SetFlashMemorySpeed(SCS_x, RCC_CFGR_HPRE_DIV1);
             RCC->CFGR |= RCC_CFGR_HPRE_DIV1;                            // AHB Prescaler DIV1
             RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE_DIV2;                    // HSE DIV2 into PLL Src Mux
             RCC->CFGR |= (SCS_x / 4000000 - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier 
@@ -59,8 +60,9 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
         case SCS_14MHz: //PLLMULL7
         case SCS_18MHz: //PLLMULL9
         case SCS_22MHz: //PLLMULL11     
-        case SCS_26MHz: //PLLMULL13     //Broken
-        case SCS_30MHz: //PLLMULL15     //Broken 
+        case SCS_26MHz: //PLLMULL13
+        case SCS_30MHz: //PLLMULL15 
+            SetFlashMemorySpeed(SCS_x, RCC_CFGR_HPRE_DIV2);
             RCC->CFGR |= RCC_CFGR_HPRE_DIV2;                            // AHB Prescaler DIV2
             RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE_DIV2;                    // HSE DIV2 into PLL Src Mux
             RCC->CFGR |= (SCS_x / 2000000 - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier
@@ -72,14 +74,16 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
         case SCS_7MHz:  //PLLMULL7
         case SCS_9MHz:  //PLLMULL9
         case SCS_11MHz: //PLLMULL11
-        case SCS_13MHz: //PLLMULL13     //Broken
-        case SCS_15MHz: //PLLMULL15     //Broken
+        case SCS_13MHz: //PLLMULL13
+        case SCS_15MHz: //PLLMULL15
+            SetFlashMemorySpeed(SCS_x, RCC_CFGR_HPRE_DIV4);
             RCC->CFGR |= RCC_CFGR_HPRE_DIV4;                            // AHB Prescaler DIV4
             RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE_DIV2;                    // HSE DIV2 into PLL Src Mux
             RCC->CFGR |= (SCS_x / 1000000 - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier
         break;
 
         case SCS_1MHz:  //PLLMULL2  // Somehow gives 8MHz
+            SetFlashMemorySpeed(SCS_x, RCC_CFGR_HPRE_DIV8);
             RCC->CFGR |= RCC_CFGR_HPRE_DIV8;                            // AHB Prescaler DIV8
             RCC->CFGR |= RCC_CFGR_PLLXTPRE_HSE_DIV2;                    // HSE DIV2 into PLL Src Mux
             RCC->CFGR |= (SCS_x / 500000  - 2) << RCC_CFGR_PLLMULL_Pos; // Converts SCS into the PLL Multiplier
@@ -91,9 +95,9 @@ void CLOCK_SetSystemClockSpeed(SystemClockSpeed SCS_x){
 
     RCC->CFGR |= SCS_x > SCS_36MHz ? RCC_CFGR_PPRE1_DIV2 : RCC_CFGR_PPRE1_DIV1; // Sets the APB1 Prescaler based on SCS
 
-    DisableHSI_EnablePLL(); //Enables the PLL
-    TIMER_Init(SCS_x);
-    currentSCS = SCS_x; // Set currentSCS to SCS_x when clocks are done configuring
+    DisableHSI_EnablePLL(); // Enables the PLL
+    TIMER_Init(SCS_x);      // Sets SysTick's Clock Speed to SCS_x (used for Millis and Delay)
+    currentSCS = SCS_x;     // Set currentSCS to SCS_x when clocks are done configuring
 }
 
 SystemClockSpeed CLOCK_GetSystemClockSpeed(void){
@@ -151,7 +155,23 @@ static void EnableHSI_DisablePLL(void){
     RCC->CR &= ~RCC_CR_PLLON;           // Disable the PLL
 }
 
-/* DELETE CODE BELOW */
-//RCC->CFGR |= RCC_CFGR_PLLMULL9 | RCC_CFGR_ADCPRE_DIV6;  // PLL Mult = 9, ADC to 12Mhz    
-//RCC->CFGR |= RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_PPRE2_DIV1; // Set APB1 Peripheral to 36Mhz, Set APB2 Peripheral to 72Mhz
-/* DELETE CODE ABOVE*/
+static void SetFlashMemorySpeed(SystemClockSpeed SCS_x, uint32_t RCC_CFGR_HPRE_x){
+    if(RCC_CFGR_HPRE_x == RCC_CFGR_HPRE_1){                 // AHB Prescaler is 1
+        CLEAR_BIT(FLASH->ACR, FLASH_ACR_PRFTBE);            // Disable the prefetch buffer
+        while((READ_BIT(FLASH->ACR, FLASH_ACR_PRFTBS))){}   // Wait until the buffer is disabled
+    }  
+    else{                                                   // AHB Prescaler is 2 or 3
+        SET_BIT(FLASH->ACR, FLASH_ACR_PRFTBE);              // Enable the prefetch buffer
+        while(!(READ_BIT(FLASH->ACR, FLASH_ACR_PRFTBS))){}  // Wait until the buffer is enabled
+    }
+
+    uint32_t flashLatency;  //Desired flash latency {See RM-58}
+    if(SCS_x <= SCS_24MHz)
+        flashLatency = FLASH_ACR_LATENCY_0;
+    else if(SCS_x <= FLASH_ACR_LATENCY_1)
+        flashLatency = FLASH_ACR_LATENCY_1;
+    else
+        flashLatency = FLASH_ACR_LATENCY_2;
+
+    MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY_Msk, flashLatency);
+}
