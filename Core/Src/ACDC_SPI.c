767784e6f2772723b9842be51cafa5a5bfa8a4a5
Devin Marx <Devin.Marx3@gmail.com>
2024-02-15 13:03:10 -0600

    Implemented the BaudRateDivider, BitMode, and LsbFirst functions. Also changed SPI_Initialize to SPI_Init to conform with other files

--- Core/Src/ACDC_SPI.c
+++ Core/Src/ACDC_SPI.c
@@ -1,34 +1,20 @@
 /**
  * @file ACDC_SPI.h
- * @author Liam Bramley
+ * @author Liam Bramley, Devin Marx
  * @brief Implementation of SPI driver
  * 
  * This file includes functions to initalize the SPI, transfer and recieve data.
  * 
  * @version 0.1
  * @date 2024-01-30
- * @copyright Copyright (c) 2023-2024
  * 
+ * @copyright Copyright (c) 2023-2024
  */
 
 #include "ACDC_SPI.h"
 
-
-/*
-{
-  __IO uint32_t CR1;      /* SPI control register 1         */
-  __IO uint32_t CR2;      /* SPI control register 2         */
-  __IO uint32_t SR;       /* SPI status register            */
-  __IO uint32_t DR;       /* SPI data register              */
-  __IO uint32_t CRCPR;    /* SPI CRC polynomial register    */
-  __IO uint32_t RXCRCR;   /* SPI RX CRC register            */
-  __IO uint32_t TXCRCR;   /* SPI TX CRC register            */
-  __IO uint32_t I2SCFGR;  /* SPI_I2S configuration register 
- SPI_TypeDef;
-*/
-
-void SPI_Initalize(const SPI_TypeDef *SPI) {
-    if(SPI == SPI1){
+void SPI_Init(SPI_TypeDef *SPIx) {
+    if(SPIx == SPI1){
         // Enable clock for SPI
         RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
@@ -41,16 +27,8 @@ void SPI_Initalize(const SPI_TypeDef *SPI) {
 
         // Set PA7 (MOSI) as alternate function push-pull output
         GPIO_PinDirection(GPIOA, GPIO_PIN_7, GPIO_MODE_OUTPUT_SPEED_50MHz, GPIO_CNF_OUTPUT_AF_PUSH_PULL);
-
-        // Configure SPI1
-        SPI1->CR1 = 0; // Clear CR1 register
-        SPI1->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_BR_0 | SPI_CR1_BR_1; // Set master mode, software NSS, baud rate
-        SPI1->CR1 |= SPI_CR1_MSTR; // Set as master
-
-        // Enable SPI1
-        SPI1->CR1 |= SPI_CR1_SPE;
     }
-    else if(SPI == SPI2){
+    else if(SPIx == SPI2){
         // Enable clock for SPI2
         RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
 
@@ -63,34 +41,67 @@ void SPI_Initalize(const SPI_TypeDef *SPI) {
 
         // Set PB15 (MOSI) as alternate function push-pull output
         GPIO_PinDirection(GPIOB, GPIO_PIN_15, GPIO_MODE_OUTPUT_SPEED_50MHz, GPIO_CNF_OUTPUT_AF_PUSH_PULL);
-
-        // Configure SPI2
-        SPI2->CR1 = 0; // Clear CR1 register
-        SPI2->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI | SPI_CR1_BR_0 | SPI_CR1_BR_1; // Set master mode, software NSS, baud rate
-        SPI2->CR1 |= SPI_CR1_MSTR; // Set as master
-
-        // Enable SPI2
-        SPI2->CR1 |= SPI_CR1_SPE;
     }
+
+    // Configure SPIx
+    SPIx->CR1 &= ~(SPI_CR1_SPE);            // Disable SPIx
+    SPIx->CR1 |= SPI_CR1_SSM | SPI_CR1_SSI; // Set master mode, software NSS
+    SPI_SetBaudDivider(SPIx, SPI_BAUD_DIV_2);
+    SPI_SetBitMode(SPIx, SPI_MODE_16Bit);
+    SPI_SetLsbFirst(SPIx, false);
+    SPIx->CR1 |= SPI_CR1_MSTR; // Set as master
+
+    // Enable SPI1
+    SPIx->CR1 |= SPI_CR1_SPE;
 }
 
-void SPI_Transmit(SPI_TypeDef *SPI, uint16_t data) {
+void SPI_Transmit(SPI_TypeDef *SPIx, uint16_t data) {
     // Wait until transmit buffer is empty
-    while (!(SPI->SR & SPI_SR_TXE));
+    while (!(SPIx->SR & SPI_SR_TXE));
 
     // Send data
-    SPI->DR = data;
+    SPIx->DR = data;
 }
 
-uint16_t SPI_Receive(const SPI_TypeDef *SPI) {
+uint16_t SPI_Receive(const SPI_TypeDef *SPIx) {
     // Wait until receive buffer is full
-    while (!(SPI->SR & SPI_SR_RXNE));
+    while (!(SPIx->SR & SPI_SR_RXNE));
 
     // Return received data
-    return SPI->DR;
+    return SPIx->DR;
 }
 
-uint16_t SPI_TransmitReceive(const SPI_TypeDef *SPI, uint16_t data) {
-    SPI_Transmit(data, SPI);
-    SPI_Receive(SPI);
+uint16_t SPI_TransmitReceive(SPI_TypeDef *SPIx, uint16_t data) {
+    SPI_Transmit(SPIx, data);
+    return SPI_Receive(SPIx);
+}
+
+void SPI_SetBaudDivider(SPI_TypeDef *SPIx, SPI_BaudDivider SPI_BAUD_DIV_x){
+    while(READ_BIT(SPIx->SR, SPI_SR_BSY)){}                 // While SPIx is busy in communication or Tx buffer is not empty
+    CLEAR_BIT(SPIx->CR1, SPI_CR1_BR_Msk);                   // Clear the SPIx Baud Divisor bits
+    SET_BIT(SPIx->CR1, SPI_BAUD_DIV_x << SPI_CR1_BR_Pos);   // Set the SPIx Baud Divisor bits
+}
+
+void SPI_SetBitMode(SPI_TypeDef *SPIx, SPI_BitMode SPI_MODE_x){
+    //Note: This bit should be written only when SPI is disabled (SPE = ‘0’) for correct operation {See RM-742}
+    bool wasInitialized = READ_BIT(SPIx->CR1, SPI_CR1_SPE) ? true : false;
+    if(wasInitialized)                      // If the peripheral was already initialized
+        CLEAR_BIT(SPIx->CR1, SPI_CR1_SPE);  // Disable SPIx
+    
+    if(SPI_MODE_x == SPI_MODE_8Bit)         // If the user wants 8-bit mode
+        CLEAR_BIT(SPIx->CR1, SPI_CR1_DFF);  // Set to 8-bit mode
+    else if(SPI_MODE_x == SPI_MODE_16Bit)   // If the user wants 16-bit mode
+        SET_BIT(SPIx->CR1, SPI_CR1_DFF);    // Set to 16-bit mode
+
+    if(wasInitialized)                      // If the peripheral was already initialized 
+        SET_BIT(SPIx->CR1, SPI_CR1_SPE);    // Enable SPIx
+}
+
+void SPI_SetLsbFirst(SPI_TypeDef *SPIx, bool LsbFirst){
+    //Note: This bit should not be changed when communication is ongoing {See RM-743}
+    while(READ_BIT(SPIx->SR, SPI_SR_BSY)){}     // While SPIx is busy in communication or Tx buffer is not empty
+    if(LsbFirst)
+        SET_BIT(SPIx->CR1  , SPI_CR1_LSBFIRST); // Set LSB first
+    else
+        CLEAR_BIT(SPIx->CR1, SPI_CR1_LSBFIRST); // Set MSB first
 }
