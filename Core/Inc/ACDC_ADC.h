3932414c1aef9221565bc0a4db99f2601d5fd758
liamster225 <liamster225@gmail.com>
2024-02-21 15:28:58 -0600

    ADC Driver

---
+++ Core/Inc/ACDC_ADC.h
@@ -0,0 +1,11 @@
+/**
+ * @file ACDC_ADC.h
+ * @author Liam Bramley
+ * @brief Header file for ADC driver
+ * @version 0.1
+ * @date 2024-01-30
+ * @copyright Copyright (c) 2023-2024
+ * 
+ */
+
+#include "stm32f1xx.h"
---
+++ Core/Src/ACDC_ADC.c
@@ -0,0 +1,23 @@
+/**
+ * @file ACDC_ADC.h
+ * @author Liam Bramley
+ * @brief Implementation of ADC driver
+ * @version 0.1
+ * @date 2024-01-30
+ * @copyright Copyright (c) 2023-2024
+ * 
+ */
+
+#include "ACDC_ADC.h"
+#include "ACDC_SPI.h"
+#include "ACDC_GPIO.h"
+
+void ADC_Recieve(uint16_t *data){
+    delay(1);
+    data = SPI_Receive(SPI2);
+    GPIO_Set(GPIOC, GPIO_PIN_7);
+}
+
+void ADC_Send(){
+    GPIO_Clear(GPIOC, GPIO_PIN_7);
+}
