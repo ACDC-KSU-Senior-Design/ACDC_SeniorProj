c1db4a66569ac34dafbcbbcc1537044d104303fe
DevinJM3 <106412270+DevinJM3@users.noreply.github.com>
2024-04-24 19:27:29 -0500

    Added PWM functionality to ACDC_TIMER.h

--- Core/Inc/ACDC_TIMER.h
+++ Core/Inc/ACDC_TIMER.h
@@ -18,6 +18,42 @@
 
 #include "ACDC_CLOCK.h"
 #include "ACDC_stdint.h"
+#include "ACDC_stdbool.h"
+
+typedef struct {
+    TIM_TypeDef *TIMx;      /**< Pointer to the TIMx peripheral */
+    uint8_t TimerChannel;   /**< Timer channel number */
+    GPIO_TypeDef *GPIOx;    /**< GPIO port for TIMx and TimerChannel */
+    uint16_t GPIO_PIN_x;    /**< GPIO pin for TIMx and TimerChannel*/
+    bool isRM;              /**< True this is a remapped pin, false if it is the defualt pin function */
+} TIMx_CHx;
+
+typedef enum {
+    PWM_MODE_1 = 0b01100000,    /**< Each PWM Cycle looks like this: ▔▔▔┃▂▂▂┃ (Starts High then transitions to Low)*/
+    PWM_MODE_2 = 0b01110000     /**< Each PWM Cycle looks like this: ▂▂▂┃▔▔▔┃ (Starts Low then transitions to High)*/
+} PWM_MODE;
+
+// Does not include Alternate function 
+#define TIM1_CH1_PB13 ((TIMx_CHx){TIM1, 1, GPIOB, GPIO_PIN_13, false})     // Timer 1 Channel 1 on GPIOB Pin 13
+#define TIM1_CH2_PB14 ((TIMx_CHx){TIM1, 2, GPIOB, GPIO_PIN_14, false})     // Timer 1 Channel 2 on GPIOB Pin 14
+#define TIM1_CH3_PA10 ((TIMx_CHx){TIM1, 3, GPIOA, GPIO_PIN_10, false})     // Timer 1 Channel 3 on GPIOA Pin 10
+#define TIM1_CH4_PA11 ((TIMx_CHx){TIM1, 4, GPIOA, GPIO_PIN_11, false})     // Timer 1 Channel 4 on GPIOA Pin 11
+
+#define TIM2_CH1_PA0 ((TIMx_CHx){TIM2, 1, GPIOA, GPIO_PIN_0, false})       // Timer 2 Channel 1 on GPIOA Pin 0
+#define TIM2_CH2_PA1 ((TIMx_CHx){TIM2, 2, GPIOA, GPIO_PIN_1, false})       // Timer 2 Channel 2 on GPIOA Pin 1
+#define TIM2_CH3_PA2 ((TIMx_CHx){TIM2, 3, GPIOA, GPIO_PIN_2, false})       // Timer 2 Channel 3 on GPIOA Pin 2
+#define TIM2_CH4_PA3 ((TIMx_CHx){TIM2, 4, GPIOA, GPIO_PIN_3, false})       // Timer 2 Channel 4 on GPIOA Pin 3
+ 
+#define TIM3_CH1_PA6 ((TIMx_CHx){TIM3, 1, GPIOA, GPIO_PIN_6, false})       // Timer 3 Channel 1 on GPIOA Pin 6
+#define TIM3_CH2_PA7 ((TIMx_CHx){TIM3, 2, GPIOA, GPIO_PIN_7, false})       // Timer 3 Channel 2 on GPIOA Pin 7
+#define TIM3_CH3_PB0 ((TIMx_CHx){TIM3, 3, GPIOB, GPIO_PIN_0, false})       // Timer 3 Channel 3 on GPIOB Pin 0
+#define TIM3_CH4_PB1 ((TIMx_CHx){TIM3, 4, GPIOB, GPIO_PIN_1, false})       // Timer 3 Channel 4 on GPIOB Pin 1
+
+#define TIM4_CH1_PB6 ((TIMx_CHx){TIM4, 1, GPIOB, GPIO_PIN_6, false})       // Timer 4 Channel 1 on GPIOB Pin 6
+#define TIM4_CH2_PB7 ((TIMx_CHx){TIM4, 2, GPIOB, GPIO_PIN_7, false})       // Timer 4 Channel 2 on GPIOB Pin 7
+#define TIM4_CH3_PB8 ((TIMx_CHx){TIM4, 3, GPIOB, GPIO_PIN_8, false})       // Timer 4 Channel 3 on GPIOB Pin 8
+#define TIM4_CH4_PB9 ((TIMx_CHx){TIM4, 4, GPIOB, GPIO_PIN_9, false})       // Timer 4 Channel 4 on GPIOB Pin 9
+
 
 /// @brief (SHOULD NOT BE CALLED BY USER, CALLED IN ACDC_CLOCK.h) Initilizes the SysClock timer and sets the value of SysClock->LOAD
 /// @param SCS_x System Clock Speed (Ex. SCS_72Mhz, SCS_36Mhz, ...)
@@ -26,6 +62,22 @@ void TIMER_Init(SystemClockSpeed SCS_x);
 /// @brief (SHOULD NOT BE CALLED BY USER, CALLED IN ACDC_CLOCK.h) Sets the value for SysClock->LOAD
 /// @param SCS_x System Clock Speed (Ex. SCS_72Mhz, SCS_36Mhz, ...)
 void TIMER_SetSystemClockSpeed(SystemClockSpeed SCS_x);
+
+/// @brief Initializes PWM output on the specified timer and channel with the given parameters. (Default duty cycle = 0)
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @param PWM_MODE_x Desired PWM mode
+/// @param frequency  Desired frequency (in Hz) for PWM to run at
+void TIMER_PWM_Init(TIMx_CHx TIMx_CHx_Pxx, PWM_MODE PWM_MODE_x, uint32_t frequency);
+
+/// @brief Sets the duty cycle of the PWM signal on the specified timer and channel.
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @param dutyCycle Duty cycle value in timer ticks.
+void TIMER_PWM_SetDuty(TIMx_CHx TIMx_CHx_Pxx, uint32_t dutyCycle);
+
+/// @brief Retrieves the period of the PWM signal on the specified timer
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @return Period of the PWM signal in timer ticks.
+uint32_t TIMER_PWM_GetPeriod(TIMx_CHx TIMx_CHx_Pxx);
 
 /// @brief Grabs and returns the total number of milliseconds since the MCU turned on
 /// @return Number of milliseconds since startup
--- Core/Src/ACDC_TIMER.c
+++ Core/Src/ACDC_TIMER.c
@@ -9,6 +9,7 @@
  */
 
 #include "ACDC_TIMER.h"
+#include "ACDC_GPIO.h"
 #include "ACDC_INTERRUPT.h"
 #include "stm32f1xx.h"
 
@@ -18,6 +19,28 @@
 volatile static uint64_t SysTickCounter;
 static uint8_t SCS_IN_MHz;                  // Clock frequency in MHz (SCS_72Mhz -> 72, SCS_36Mhz -> 36, Etc.)
 
+#pragma region PRIVATE_FUNCTION_PROTOTYPES
+/// @brief Enables the TIMx peripheral clock
+/// @param TIMx Timer to initialize clock for (Ex. TIM1, TIM2, ...)
+static void TIMER_InitClk(const TIM_TypeDef *TIMx);
+
+/// @brief Sets the PWM mdoe for the specified timer and channel
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @param PWM_MODE_x Desired PWM mode (Ex. PWM_MODE_1 or PWM_MODE_2)
+static void TIMER_PWM_SetPwmMode(TIMx_CHx TIMx_CHx_Pxx, PWM_MODE PWM_MODE_x);
+
+/// @brief Enables or disables the output signal for the specified timer and channel.
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @param enable True if you want to enable the output signal, else false to disable
+static void TIMER_PWM_SetOutputEnable(TIMx_CHx TIMx_CHx_Pxx, bool enable);
+
+/// @brief Enables or disables preloading the output for the specified timer and channel.
+/// @param TIMx_CHx_Pxx Struct containing configuration data for the specific timer and channel
+/// @param enable True if you want to enable preloading the output, else false to disable
+static void TIMER_PWM_SetPreloadEnable(TIMx_CHx TIMx_CHx_Pxx, bool enable);
+#pragma endregion
+
+#pragma region PUBLIC_FUNCTIONS
 void TIMER_Init(SystemClockSpeed SCS_x){  //TODO: Should only ever be called once (Need to add an assert)
     SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk;
     TIMER_SetSystemClockSpeed(SCS_x);
@@ -27,6 +50,43 @@ void TIMER_Init(SystemClockSpeed SCS_x){  //TODO: Should only ever be called onc
 void TIMER_SetSystemClockSpeed(SystemClockSpeed SCS_x){
     SCS_IN_MHz = SCS_x / MS_PER_SECOND / US_PER_MS;     // Divide by 1,000,000 (72MHz -> 72)
     SysTick->LOAD = ((SCS_x / MS_PER_SECOND) - 1) & SysTick_LOAD_RELOAD_Msk;
+}
+
+void TIMER_PWM_Init(TIMx_CHx TIMx_CHx_Pxx, PWM_MODE PWM_MODE_x, uint32_t frequency){
+    TIMER_InitClk(TIMx_CHx_Pxx.TIMx);                                               // Initialize the clock for the timer
+    TIMER_PWM_SetPwmMode(TIMx_CHx_Pxx, PWM_MODE_x);                                 // Set the mode to PWM1 or PWM2
+    TIMER_PWM_SetDuty(TIMx_CHx_Pxx, 0);                                             // Set the default duty cycle to 0
+    TIMER_PWM_SetOutputEnable(TIMx_CHx_Pxx, true);                                  // Enable the output on the pin 
+    TIMER_PWM_SetPreloadEnable(TIMx_CHx_Pxx, true);                                 // Enable the output compare preloader
+    WRITE_REG(TIMx_CHx_Pxx.TIMx->PSC, 0);                                           // Set Prescaler to DIV 1 (Can change later if need more functionality {See RM-356})
+    WRITE_REG(TIMx_CHx_Pxx.TIMx->ARR, (CLOCK_GetSystemClockSpeed() / frequency)-1); // Set the PWM Period (Subtract 1 because its 0 based)
+
+    if(TIMx_CHx_Pxx.TIMx == TIM1)                           // If it is an advanced timer (1 or 8)
+        SET_BIT(TIMx_CHx_Pxx.TIMx->BDTR, TIM_BDTR_MOE);     // Enable the master output 
+    
+    //GPIO Configuration {See RM-166}
+    GPIO_PinDirection(TIMx_CHx_Pxx.GPIOx, TIMx_CHx_Pxx.GPIO_PIN_x, GPIO_MODE_OUTPUT_SPEED_50MHz, GPIO_CNF_OUTPUT_AF_PUSH_PULL);   
+
+    SET_BIT(TIMx_CHx_Pxx.TIMx->CR1, TIM_CR1_CEN);
+}
+
+void TIMER_PWM_SetDuty(TIMx_CHx TIMx_CHx_Pxx, uint32_t dutyCycle){
+    if(dutyCycle > TIMER_PWM_GetPeriod(TIMx_CHx_Pxx))   // If dutyCycle is bigger than 100% duty cycle
+        dutyCycle = TIMER_PWM_GetPeriod(TIMx_CHx_Pxx);  // Set it to the max duty cycle
+
+    uint8_t channel = TIMx_CHx_Pxx.TimerChannel;        // Grab the specific channel for the timer
+    if(channel == 1)                                    // Check the channel
+        TIMx_CHx_Pxx.TIMx->CCR1 = dutyCycle;            // Set the duty cycle for the specific channel
+    else if(channel == 2)
+        TIMx_CHx_Pxx.TIMx->CCR2 = dutyCycle;
+    else if(channel == 3)
+        TIMx_CHx_Pxx.TIMx->CCR3 = dutyCycle;
+    else if(channel == 4)
+        TIMx_CHx_Pxx.TIMx->CCR4 = dutyCycle;
+}
+
+uint32_t TIMER_PWM_GetPeriod(TIMx_CHx TIMx_CHx_Pxx){
+    return TIMx_CHx_Pxx.TIMx->ARR;
 }
 
 void SysTick_Handler(void){
@@ -51,3 +111,62 @@ void Delay_US(uint64_t delayVal){
     uint64_t current = Micros();
     while(Micros() - current <= delayVal){}
 }
+#pragma endregion
+
+#pragma region PRIVATE_FUNCTOINS
+static void TIMER_InitClk(const TIM_TypeDef *TIMx){
+    // TIM1 is the only timer that is located on APB2   
+    // TIM2, TIM3, & TIM4 are located on APB1   {See RM-93, RM-113, RM-117}
+    if(TIMx == TIM1)
+        SET_BIT(RCC->APB2ENR, RCC_APB2ENR_TIM1EN);
+    else if(TIMx == TIM2)
+        SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM2EN);
+    else if(TIMx == TIM3)
+        SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM3EN);
+    else if(TIMx == TIM4)
+        SET_BIT(RCC->APB1ENR, RCC_APB1ENR_TIM4EN);
+}
+
+static void TIMER_PWM_SetPwmMode(TIMx_CHx TIMx_CHx_Pxx, PWM_MODE PWM_MODE_x){
+    // CCMR1 and CCMR2 have the same bit structure
+    volatile uint16_t *CCMRx = TIMx_CHx_Pxx.TimerChannel < 3 ?  // If it is channel 1 or 2
+                                   &TIMx_CHx_Pxx.TIMx->CCMR1 :  // Grab CCMR1
+                                   &TIMx_CHx_Pxx.TIMx->CCMR2;   // Else grab CCMR2
+    
+    if(TIMx_CHx_Pxx.TimerChannel == 1 || TIMx_CHx_Pxx.TimerChannel == 3){   // if it is the first 8 bits on CCMR1 or CCMR2 (CH1 or CH3)
+        CLEAR_BIT(*CCMRx, TIM_CCMR1_OC1M_Msk);      // Clear the OCxM bits
+        SET_BIT(*CCMRx, PWM_MODE_x);                // Set the desired PWM mode
+    } else {                                                                // else its the second 8 bits of CCMR1 or CCMR2 (CH2 or CH4)
+        CLEAR_BIT(*CCMRx, TIM_CCMR1_OC1M_Msk << 8); // Clear the upper OCxM bits
+        SET_BIT(*CCMRx, PWM_MODE_x << 8);           // Set the upper PWM mode bits
+    }
+}
+
+static void TIMER_PWM_SetOutputEnable(TIMx_CHx TIMx_CHx_Pxx, bool enable){
+    uint16_t bitOffset = (1 << (TIMx_CHx_Pxx.TimerChannel-1) * 4); // using the channel it calculates CCERx
+
+    if(enable)
+        SET_BIT(TIMx_CHx_Pxx.TIMx->CCER, bitOffset);
+    else 
+        CLEAR_BIT(TIMx_CHx_Pxx.TIMx->CCER, bitOffset);
+}
+
+static void TIMER_PWM_SetPreloadEnable(TIMx_CHx TIMx_CHx_Pxx, bool enable){
+    // CCMR1 and CCMR2 have the same bit structure
+    volatile uint16_t *CCMRx = TIMx_CHx_Pxx.TimerChannel < 3 ?  // If it is channel 1 or 2
+                                   &TIMx_CHx_Pxx.TIMx->CCMR1 :  // Grab CCMR1
+                                   &TIMx_CHx_Pxx.TIMx->CCMR2;   // Else grab CCMR2
+    
+    if(enable){
+        if(TIMx_CHx_Pxx.TimerChannel == 1 || TIMx_CHx_Pxx.TimerChannel == 3)   // if it is the first 8 bits on CCMR1 or CCMR2 (CH1 or CH3)
+            SET_BIT(*CCMRx, TIM_CCMR1_OC1PE);
+        else                                                                   // else its for the second 8 bits of CCMR1 or CCMR2 (CH2 or CH4)
+            SET_BIT(*CCMRx, TIM_CCMR1_OC2PE);        
+    } else {
+        if(TIMx_CHx_Pxx.TimerChannel == 1 || TIMx_CHx_Pxx.TimerChannel == 3)   
+            CLEAR_BIT(*CCMRx, TIM_CCMR1_OC1PE);
+        else                                                                  
+            CLEAR_BIT(*CCMRx, TIM_CCMR1_OC2PE);  
+    }
+}
+#pragma endregion
